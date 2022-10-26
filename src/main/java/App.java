ddbd788d5a156f9feab3c373fc5f0f3e1ddb4100
Pavel <lovecars527@gmail.com>
2022-10-27 02:44:13 +0300

    initial

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,20 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
+
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+
+            HelloWorld beanOne =
+                    (HelloWorld) applicationContext.getBean("helloworld");
+            HelloWorld beanTwo =
+                    (HelloWorld) applicationContext.getBean("helloworld");
+
+            Cat cat1 = (Cat) applicationContext.getBean("cat");
+            Cat cat2 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println("Результат сравнения бинов  \"helloworld\": " + (beanOne == beanTwo));
+
+        System.out.println("Результат сравнения бинов  \"cat\" : " + (cat1 == cat2));
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,6 @@
+public class Cat {
+    private Cat cat;
+
+    public Cat() {
+    }
+}
